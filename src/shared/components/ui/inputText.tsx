e532a3c89bb7c97ed1083ab97324bf051085f3cc
SAM <samirajetzabel@gmail.com>
2026-06-22 23:53:37 -0500

    feat(shared-components): add input text and modal component

---
+++ src/shared/components/ui/inputText.tsx
@@ -0,0 +1,15 @@
+interface InputTextProps{
+    label:string;
+    name:string;
+    placeholder?:string;
+    className?:string;
+}
+
+export function InputText({label,name, placeholder, className}: InputTextProps) {
+    return(
+        <div>
+            <h3 className="text-text-title font-medium text-sm pb-2">{label}</h3>
+            <input name={name} placeholder={placeholder} className={`${className} border-1 border-border-input rounded-lg p-2.5 w-full focus:border-accent-button focus:outline-hidden`}/>
+        </div>
+    )
+}
